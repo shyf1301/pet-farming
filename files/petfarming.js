16d58562369d8d76d1ee0defb48e6c54d0ca6248
Jared-g <jared-g@live.ca>
2018-01-27 13:35:49 -0500

    Capitalize 'Entries'

--- files/petfarming.js
+++ files/petfarming.js
@@ -185,7 +185,7 @@ function farm() {
       outputString += "&nbsp;" + addedFrags + " &nbsp; " + petArray[tierListArray[i]].name + "<br />";
     }    
   }
-  outputString += "&nbsp;" + totalEntries + " &nbsp; entries left<br />"
+  outputString += "&nbsp;" + totalEntries + " &nbsp; Entries left<br />"
   outputString += "<br /><button onclick='addFarmFrags()' style='margin-bottom:5px'>Add</button><br />"
   $("#farminglist").html(outputString);
   $("#farminglist").css("border","1px solid #555");
